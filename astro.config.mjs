76de00f54e0d077cbce9aec182bd183458675f6d
Sebastian Sepulveda <sesepulveda@ug.uchile.cl>
2025-02-16 23:02:13 -0300

    add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
+
+const site = 'https://sbsepul.github.io';
+
 export default defineConfig({
   integrations: [tailwind(), react(), mdx()],
-  site: 'https://sbsepul.github.io',
+  site: site,
   base: 'src/',
 });
